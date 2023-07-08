6ed7ce8a32139ba07af9f03f8cda243952fb28c3
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2023-07-08 14:03:01 +0100

    Completed and implemented documentation page

---
+++ script/documentations.js
@@ -0,0 +1,25 @@
+// documentations page script start
+const cardDesign = document.querySelector('.card-design')
+const cardDesignBack = document.querySelector('.back')
+const leftColor = document.querySelector('.left-color')
+const midColor = document.querySelector('.mid-color')
+const rightColor = document.querySelector('.right-color')
+const lastColor = document.querySelector('.last-color')
+
+leftColor.addEventListener('click', ()=>{
+	cardDesign.style.background = 'linear-gradient(134deg, #EB001B -43.28%, #F79E1B  97%)'
+	cardDesignBack.style.background = 'linear-gradient(134deg, #EB001B -43.28%, #F79E1B  97%)'
+})
+midColor.addEventListener('click', ()=>{
+	cardDesign.style.background = 'linear-gradient(134deg, #327A60 35.63%, #230B58 93.35%)'
+	cardDesignBack.style.background = 'linear-gradient(134deg, #327A60 35.63%, #230B58 93.35%)'
+})
+rightColor.addEventListener('click', ()=>{
+	cardDesign.style.background = 'linear-gradient(134deg, #4C0964 7.86%, #EF8B16 97%)'
+	cardDesignBack.style.background = 'linear-gradient(134deg, #4C0964 7.86%, #EF8B16 97%)'
+})
+lastColor.addEventListener('click', ()=>{
+	cardDesign.style.background = 'linear-gradient(134deg, #EB001B 20.86%, #000000 97%)'
+	cardDesignBack.style.background = 'linear-gradient(134deg, #EB001B 20.86%, #000000 97%)'
+})
+// documentations page script end
