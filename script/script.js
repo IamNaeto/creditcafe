1a50ddbef9f6e094a9857fcd30494e7c2ed1aaf5
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2023-07-09 14:10:38 +0100

    implemented dynamicness of the nav links

--- script/script.js
+++ script/script.js
@@ -6,3 +6,27 @@ hamMenu.addEventListener('click', () =>{
 	offScreenMenu.classList.toggle('active')
 })
 // Hamburger script end
+
+ // Defining logout
+ const logOutBtn = document.getElementById('signIn');
+ logOutBtn.addEventListener("click", () => {
+   localStorage.setItem('userLoggedIn', false);
+ });
+ 
+// Check if user is logged in other to add dynamicness to the nav links
+const regBtn = document.getElementById('register-link')
+const signOutBtn = document.getElementById('signIn')
+const heroBtn = document.getElementById('herobtn')
+
+if (localStorage.getItem('userLoggedIn') === "true") {
+	regBtn.innerHTML = 'Dashboard';
+	regBtn.href = 'html/dashboard.html';
+	signOutBtn.innerHTML = 'Sign Out';
+	heroBtn.href = 'html/card-gen.html';
+  }else{
+	regBtn.innerHTML = 'Register'
+	regBtn.href = 'html/sign-up.html';
+	signOutBtn.innerHTML = 'Sign In';
+	heroBtn.href = 'html/sign-in.html';
+  }
+  
